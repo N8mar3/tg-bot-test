897a57e60acafaa479b332fd646e45691f3d868d
N8mar3 <imtiredtocreate@gmail.com>
2022-12-14 18:41:12 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { useEffect } from 'react';
 
 const tg = window.Telegram.WebApp;
 
@@ -14,7 +15,10 @@ function App() {
 
   return (
     <div className="App">
-        <h1 className="headerText">Алёна Усенко</h1>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/> 
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/> 
+        <link href="https://fonts.googleapis.com/css2?family=Poiret+One&display=swap" rel="stylesheet"/>
+        <h1 className="headerText" >Алёна Усенко</h1>
         <button onClick={onClose}>Close</button>
     </div>
   );
