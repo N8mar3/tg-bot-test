87a2d894fa5d4533ee90c2b9f2621b11f7422ccf
N8mar3 <imtiredtocreate@gmail.com>
2022-12-15 12:29:39 +0800

    hook fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./header/Header";
-import { TgHook } from "./hooks/Telegram.jsx";
+
+const tg = window.Telegram.WebApp;
 
 export default function App() {
-
-  const { tg } = TgHook();
 
   useEffect(() => {
     tg.ready();
